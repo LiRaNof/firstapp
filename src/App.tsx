4516e326264c1467ecdcb9a1c092664d8f9b057c
Yuhei Sugino <sologne.01@gmail.com>
2024-12-26 20:56:48 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+
+import Home from './components/ui/home';
+import Exam from './components/ui/exam';
+
+
+export default function APP() {
+  
+  
+
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exam' element={<Exam />} />
+      </Routes>
+    </BrowserRouter>
+  );
+  
+}
