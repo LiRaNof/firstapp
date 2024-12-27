477d0e9e6f6ed7128c81968496c751ab43961a43
Yuhei Sugino <sologne.01@gmail.com>
2024-12-28 00:24:18 +0900

    talsind2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import plugins from "tailwindcss-animate"
 export default{
   darkMode: ["class"],
   content: [
@@ -88,5 +89,5 @@ export default{
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [plugins],
 }
